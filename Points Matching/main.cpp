2755ce256277f39a9c8b1d78290428f2a262f5b9
Xiaoyang Li <ldlglxy@gmail.com>
2017-06-13 08:54:50 -0400

    Opencv x86&x64
    Setting up the opencv environment

---
+++ Points Matching/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat srcImage = imread("lena.jpg");      //加载图像文件
+	namedWindow("lena", WINDOW_AUTOSIZE);   //设置显示图像的窗口标题为lena,属性为自动调整大小
+	imshow("lena", srcImage);               //显示图片
+
+	waitKey(0);
+
+	return 0;
+}
